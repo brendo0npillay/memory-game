17195b67943b063015c980fdd90b674b17537a3a
brendon9909 <brendonpillay02@gmail.com>
2025-06-20 12:36:52 +0200

    changed dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/memory_game.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
   mode: "development",
 };
